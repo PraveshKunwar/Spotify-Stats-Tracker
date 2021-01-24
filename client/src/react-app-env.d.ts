d46a9dda3ed853f9550365d58db3cb77a37a43e8
PraveshKunwar <praveshisawesome2017@gmail.com>
2021-01-23 23:31:39 -0500

    added types

--- client/src/react-app-env.d.ts
+++ client/src/react-app-env.d.ts
@@ -3,3 +3,6 @@ declare module "*.css";
 declare module "*.scss";
 declare module "*.otf";
 declare module "styled-components";
+declare module "react-router-dom";
+declare module "react-redux";
+declare module "redux";
