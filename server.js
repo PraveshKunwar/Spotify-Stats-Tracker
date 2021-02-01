0dbc3c9e77c37b87d801b4d6bfd3188a115abfd7
PraveshKunwar <praveshisawesome2017@gmail.com>
2021-02-01 17:34:51 -0500

    Prepare for deployment

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/client/build"));
+  app.use(express.static("client/build"));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
